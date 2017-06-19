67ed973bd91c1922acee3eb3164eb37bba708d65
Vinayak Shekhar <vshekhar@gmail.com>
2017-06-18 22:17:30 -0400

    Updated html and some clean up

--- index.js
+++ index.js
@@ -1,48 +1,60 @@
 var express = require('express')
 var app = express()
 var gpio = require('rpi-gpio');
-gpio.setup(7, gpio.DIR_OUT);
+
+var pin = 7;
+var runInMinutes = 15;
+var html = 
+  `<html>
+    <head><meta name="viewport" content="width=device-width, initial-scale=1.0"></head>
+    <body>
+      <h1>Welcome to Garden Watering System</h1>
+      <h2><a href=/on>On</a></h2>
+      <h2><a href=/off>Off</a></h2>
+      <h3><a href=/check>Check</a></h3>
+    </body>
+  </html>`;
+
+gpio.setup(pin, gpio.DIR_OUT);
 
 app.get('/', function(req, res) {
-  res.send('<html><body><a href=/on>On</a><br><a href=/off>Off</a><br><a href=/check>Check</a><br></body></html>')
+  res.send(html)
 });
 
 app.get('/on', function(req, res) {
-  gpio.read(7, function(err, value) {
+  gpio.read(pin, function(err, value) {
     if(value) {
-      res.send('Water already on');
+      res.send(html+'Water already on');
     } else {
-      gpio.write(7, true, function(err) {
+      gpio.write(pin, true, function(err) {
         if (err) throw err;
 
         res.send('Starting Water');
         console.log('Written to pin');
 
         setTimeout(function() {
-          gpio.write(7, false, function(err) {
+          gpio.write(pin, false, function(err) {
             console.log('Turning water off');
           });
-        }, 15*60*1000);
-
+        }, runInMinutes*60*1000);
       });
     }
   });
 })
 
 app.get('/off', function(req, res) {
-    gpio.write(7, false, function(err) {
+    gpio.write(pin, false, function(err) {
       if (err) throw err;
-
-      res.send('Stopping Water')
+      res.send(html+'Stopping Water')
       console.log('Written to pin');
-
     });
 })
 
 app.get('/check', function(req, res) {
-    gpio.read(7, function(err, value) {
+    gpio.read(pin, function(err, value) {
       console.log('The value is ' + value);
-      res.send(value);
+      var message = value ? "On" : "Off";
+      res.send(html + "Water is " + message);
     });
 });
 
